998a343535405bca4f116c208b5bbe42a0da4850
dnam7474 <Daniel.Nam@cvent.com>
2025-04-24 15:43:34 -0400

    Initial commit

---
+++ food-app/src/App.tsx
@@ -0,0 +1,10 @@
+import'./App.css';
+import MenuItemList from './components/MenuItemList';
+
+export default function App() {
+  return (
+    <div className={'app'}>
+      <MenuItemList/>
+    </div>
+  );
+}
---
+++ food-app/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface MenuItem {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+  }
+
+  export interface CartItem extends MenuItem {
+    quantity: number;
+  }
